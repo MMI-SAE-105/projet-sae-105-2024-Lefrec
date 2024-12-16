14a18fb193c52ad724ab6d9e0994f41eb056abb6
Lefrec <blackfox244@gmail.com>
2024-12-16 15:49:36 +0100

    système de menu non complet

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,19 @@
 const menu = document.querySelector(".menu");
+const subMenu = document.querySelector(".sous-menu");
 const btn = document.querySelector(".header__menu-btn");
+const subBtn = document.querySelector("#btnSub");
 const icon = document.querySelector(".header__menu-icon");
 
 
 btn.addEventListener("click", (event) => {
-    const isOpen = btn.ariaExpanded=="true";
-    console.log(isOpen);
-    console.log(!isOpen);
+    let isOpen = btn.ariaExpanded=="true";
     btn.ariaExpanded = !isOpen;
     menu.ariaHidden = isOpen;
     document.body.classList.toggle("noscroll");
+})
+
+subBtn.addEventListener("click", (event) => {
+    let isOpen = subBtn.ariaExpanded=="true";
+    subBtn.ariaExpanded = !isOpen;
+    subMenu.ariaHidden = isOpen;
 })
